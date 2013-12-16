e6c53597f279c5502af60b2bc1c2696eadb00df1
kennethjiang <kenneth.jiang@gmail.com>
2013-12-16 18:38:50 +0000

    contest controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Main::Application.routes.draw do
+  get "contest/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
