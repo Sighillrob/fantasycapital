0d21c50454340c2b62a4c01161834063c4b3ea0a
kennethjiang <kenneth.jiang@gmail.com>
2013-12-18 00:58:32 +0000

    change route to use resource for Contests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Main::Application.routes.draw do
   devise_for :users
-  get "contest/index"
+  resources :contests
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/contests_spec.rb
+++ spec/requests/contests_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
-describe "Contests," do
-  describe "Contests" do
-    it "GET /contests/index" do
-      get "/contest/index"
+describe "Contests" do
+  describe "GET /contests" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get contests_path
       response.status.should be(200)
     end
   end
