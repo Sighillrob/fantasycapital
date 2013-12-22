8800afced94a06759b8d54c24fbcad0075b1fed8
Kenneth <kenneth@opsvisibility.com>
2013-12-22 18:30:38 +0000

    fix column name entry_fee

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131220000001) do
+ActiveRecord::Schema.define(version: 20131222181938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,22 +21,18 @@ ActiveRecord::Schema.define(version: 20131220000001) do
     t.string   "sport"
     t.string   "contest_type"
     t.decimal  "prize"
-    t.decimal  "enrtry_fee"
+    t.decimal  "entry_fee"
     t.datetime "contest_start"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "entries", force: true do |t|
+  create_table "entries", id: false, force: true do |t|
     t.integer  "user_id"
     t.integer  "contest_id"
-    t.string   "sport"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "entries", ["contest_id"], name: "index_entries_on_contest_id", using: :btree
-  add_index "entries", ["user_id"], name: "index_entries_on_user_id", using: :btree
 
   create_table "player_stats", force: true do |t|
     t.integer  "player_id"
