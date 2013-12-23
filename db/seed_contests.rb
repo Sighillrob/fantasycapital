cd3cf0d8efa1dd8b42379c8fc8843f694f4f705c
Kenneth <kenneth@opsvisibility.com>
2013-12-23 06:31:16 +0000

    data seed re-design

--- db/seed_contests.rb
+++
@@ -1,5 +0,0 @@
-require 'csv'
-
-CSV.foreach(ARGV[0]) do |row|
-puts "Contest.create(sport: \"#{row[1]}\", contest_type: \"#{row[0]}\", enrtry_fee: #{row[3].to_f}, prize: #{row[4].to_f}, contest_start:  Time.now + 60*60*12)"
-end
