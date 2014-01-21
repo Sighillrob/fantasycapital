507b5dee1ccecb16eef85e612e5946f4267cc0c0
Kenneth <kenneth@opsvisibility.com>
2014-01-21 16:46:26 +0000

    display only today's projection

--- app/controllers/projections_controller.rb
+++ app/controllers/projections_controller.rb
@@ -3,7 +3,9 @@ class ProjectionsController < ApplicationController
 
   # GET /projections
   def index
-    @projections = Projection::Projection.includes(:player, scheduled_game: [:home_team, :away_team]).where("updated_at > ?", 1.day.ago)
+    now = Time.now.in_time_zone('America/New_York').beginning_of_day
+    time_range = now..(now+1.day)
+    @projections = Projection::Projection.includes(:player, scheduled_game: [:home_team, :away_team]).where('projection_scheduled_games.start_date' => time_range)
   end
 
 end
