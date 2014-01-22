ae596fee77fd5c8d03719bb2f2284a1c9e94dec0
craigerm <craigerm@gmail.com>
2014-01-22 16:26:39 -0500

    Rename balance to balance_in_cents

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140120215151) do
+ActiveRecord::Schema.define(version: 20140122211901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20140120215151) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "stripe_customer_id"
-    t.integer  "balance",            default: 0
+    t.integer  "balance_in_cents",   default: 0
     t.integer  "lock_version",       default: 0
   end
 
