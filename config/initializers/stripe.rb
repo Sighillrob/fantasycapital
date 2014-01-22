5496bd924ba895766cb1b67b4bacba7421f28f56
craigerm <craigerm@gmail.com>
2014-01-22 16:31:18 -0500

    Add stripe configuration

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,13 @@
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key  => ENV['STRIPE_SECRET_KEY']
+  }
+else
+  # Stripe (these are test keys only so they are safe)
+  Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_BmYJ7lge4AeP0sKdsle7K93A',
+   :secret_key => 'sk_test_EYDrd5oqAgSNqb0iynt0khNI'
+  }
+end
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
