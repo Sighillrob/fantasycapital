b85fe101c3bf8852bd00b0842b5796f2d727a446
craigerm <craigerm@gmail.com>
2014-01-22 16:39:14 -0500

    Add helper for showing states/provinces

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,4 +1,9 @@
 module AccountsHelper
+
+  def only_us_and_canada_regions
+    Carmen::Country.coded('US').subregions + Carmen::Country.coded('CA').subregions
+  end
+
   def balanced_marketplace_uri
     Rails.configuration.balanced_marketplace_uri
   end
