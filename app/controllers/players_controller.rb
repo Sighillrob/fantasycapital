82d8d1824358cbae81465a65f536114756c49df8
Alexsey Popov <alexsey.popov718@gmail.com>
2014-02-28 11:56:33 +0200

    ordering on the historical player stats

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,7 +3,7 @@ class PlayersController < ApplicationController
   	@summary = {}
   	@matchup = {}
   	@player = Player.find(params[:id])
-  	@summary_time_spans = PlayerStat.where("player_id = ? AND dimension = ?", params[:id], 'summary').group('time_span').select('time_span').all
+  	@summary_time_spans = PlayerStat.where("player_id = ? AND dimension = ?", params[:id], 'summary').group('time_span', 'display_priority').select('time_span').order('display_priority').all
   	@summary_time_spans.each do |time_span|
   		stat_line = PlayerStat.player_stat_line @player.id, 'summary', time_span.time_span
   		@summary[time_span.time_span] = stat_line
