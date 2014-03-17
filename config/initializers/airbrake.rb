b2c4059700dc4e1c69a8c8dfe402da5f7ab0dc2a
emilos <emil@ajdyna.com>
2014-03-17 23:18:06 +0100

    commenting out airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,4 @@
-Airbrake.configure do |config|
-  config.api_key = 'a847c662a8ed86b96b30e146364d6e7a'
-end
+## Nils: temporarily comment, causing tests to fail
+#Airbrake.configure do |config|
+#  config.api_key = 'a847c662a8ed86b96b30e146364d6e7a'
+#end
