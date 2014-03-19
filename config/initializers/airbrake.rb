5e349a482e1c8a5b32435a33344975d098f777b3
Kenneth <kenneth.jiang@gmail.com>
2014-03-19 13:54:39 +0000

    fix typo: staging -> production

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,4 @@
-if Rails.env.staging? or Rails.env.staging?
+if Rails.env.production? or Rails.env.staging?
   Airbrake.configure do |config|
     config.api_key = 'a847c662a8ed86b96b30e146364d6e7a'
   end
