0028abf3cb02effe6a9367b1a7ce950091b0c896
Kenneth <kenneth.jiang@gmail.com>
2014-03-19 03:22:42 +0000

    activate airbrake only when it's production or staging

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,5 @@
-## Nils: temporarily comment, causing tests to fail
-#Airbrake.configure do |config|
-#  config.api_key = 'a847c662a8ed86b96b30e146364d6e7a'
-#end
+if Rails.env.staging? or Rails.env.staging?
+  Airbrake.configure do |config|
+    config.api_key = 'a847c662a8ed86b96b30e146364d6e7a'
+  end
+end
