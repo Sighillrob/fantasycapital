bf537bee04550644a40d2589924926947f28d5e8
Nils Bunger <nilsbunger@gmail.com>
2014-04-04 16:53:18 -0700

    Fix to gamecenter query optimization, was spitting out bad scores before

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -17,5 +17,39 @@
 require 'spec_helper'
 
 describe Player do
-  it { should belong_to(:sport_position) }
+  let(:positions) { create_list(:sport_position, 6)}
+  let(:player) { create(:player, sport_position: positions[0]) }
+  let(:teams) { create_list(:team, 4)}
+
+  let!(:game) {create(:game_score, playdate:"2014-03-21", home_team: teams[0], away_team: teams[1])}
+  let!(:game2) {create(:game_score, playdate:"2014-03-22", home_team: teams[0], away_team: teams[1])}
+
+  let!(:rtscores) { [create(:player_real_time_score, player:player, game_score:game, name: "fp", value:20),
+                     create(:player_real_time_score, player:player, game_score:game2, name: "fp", value:30),] }
+
+
+  it "returns correct FP scores without eager-loading" do
+
+    should belong_to(:sport_position)
+    expect(player.realtime_fantasy_points(game.id)).to eq(20)
+    expect(player.realtime_fantasy_points(game2.id)).to eq(30)
+  end
+
+  it "returns correct FP scores WITH eager-loading" do
+    # For entering GameCenter, the entries_controller can eager-load all the player scores for the
+    # current games.
+    # Player model is sophisticated enough to detect this and skip the additional query. Check
+    # this functionality by creating players with eager-loaded scores, then getting their points.
+
+    myplayer = Player.includes(:player_real_time_scores).where(
+        player_real_time_scores: {game_score_id: [game.id]})[0]
+
+    expect(myplayer.realtime_fantasy_points).to eq(20)
+
+    myplayer2 = Player.includes(:player_real_time_scores).where(
+        player_real_time_scores: {game_score_id: [game2.id]})[0]
+
+    expect(myplayer2.realtime_fantasy_points).to eq(30)
+  end
+
 end
