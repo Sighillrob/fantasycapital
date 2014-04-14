ff48f92a6aa1ea53ac5fc160756104124daada1c
Nils Bunger <nilsbunger@gmail.com>
2014-04-14 10:09:21 -0700

    Fix creation of simulated games in 2050 so we don't get duplicate games.
    Also add validation in the models to prevent multiple games, players, teams
    with same external IDs.

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -13,8 +13,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :ext_team_id do |n|
+    "ABC#{n}-TEAM-IJKL"
+  end
+
   factory :team do
     name "MyString"
     teamalias "MyString"
+    ext_team_id
   end
 end
