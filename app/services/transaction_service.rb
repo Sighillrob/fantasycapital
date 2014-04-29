55e488b77eb989562b9faf023522d60047468afd
Jack Pien <jack@jackpien.com>
2014-04-29 13:55:13 -0700

    Add stub for transaction service to handle contest winnings, entry, canceling

---
+++ app/services/transaction_service.rb
@@ -0,0 +1,10 @@
+module TransactionService
+  def TransactionService.contest_entry( user, entry )
+  end
+
+  def TransactionService.contest_entry_cancel( user, entry )
+  end
+
+  def TransactionService.contest_end( contest )
+  end
+end
